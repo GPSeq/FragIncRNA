3056886cbd82ac753d69a814ef3b7bdbc59e7234
Ahmad lutfi <yynk1@hotmail.com>
2026-05-16 14:08:09 +0200

    bash

---
+++ bash_scripts/download_primates.sh
@@ -0,0 +1,41 @@
+#Sources: https://hgdownload.gi.ucsc.edu/hubs/primates/asmStats.html
+#Bonobos:
+#GCA_029289425.3_NHGRI_mPanPan1-v2.1_pri
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/029/289/425/GCF_029289425.2_NHGRI_mPanPan1-v2.0_pri/GCF_029289425.2_NHGRI_mPanPan1-v2.0_pri_cds_from_genomic.fna.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/029/289/425/GCF_029289425.2_NHGRI_mPanPan1-v2.0_pri/GCF_029289425.2_NHGRI_mPanPan1-v2.0_pri_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/029/289/425/GCF_029289425.2_NHGRI_mPanPan1-v2.0_pri/GCF_029289425.2_NHGRI_mPanPan1-v2.0_pri_genomic.fna.gz
+
+#GCF_000258655.2_panpan1.1
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/258/655/GCF_000258655.2_panpan1.1/GCF_000258655.2_panpan1.1_cds_from_genomic.fna.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/258/655/GCF_000258655.2_panpan1.1/GCF_000258655.2_panpan1.1_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/258/655/GCF_000258655.2_panpan1.1/GCF_000258655.2_panpan1.1_genomic.fna.gz
+
+#GCF_013052645.1_Mhudiblu_PPA_v0
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/013/052/645/GCF_013052645.1_Mhudiblu_PPA_v0/GCF_013052645.1_Mhudiblu_PPA_v0_genomic.fna.gz
+
+#Chimpanzees:
+#GCF_002880755.1_Clint_PTRv2
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/880/755/GCF_002880755.1_Clint_PTRv2/GCF_002880755.1_Clint_PTRv2_genomic.fna.gz
+
+#GCA_028858775.3_NHGRI_mPanTro3-v2.1_pri
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/028/858/775/GCF_028858775.2_NHGRI_mPanTro3-v2.0_pri/GCF_028858775.2_NHGRI_mPanTro3-v2.0_pri_genomic.fna.gz
+
+#Human:
+#GCA_054883195.1_H9_T2T.hap1
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/054/883/195/GCA_054883195.1_H9_T2T.hap1/GCA_054883195.1_H9_T2T.hap1_genomic.fna.gz
+
+#GCA_054883265.1_H9_T2T.hap2
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/054/883/265/GCA_054883265.1_H9_T2T.hap2/GCA_054883265.1_H9_T2T.hap2_genomic.fna.gz
+
+#GCA_018503265.2_NA19240_pat_hprc_f2
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/018/503/265/GCA_018503265.2_NA19240_pat_hprc_f2/GCA_018503265.2_NA19240_pat_hprc_f2_genomic.fna.gz
+
+#GCA_018503275.2_NA19240_mat_hprc_f2
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/018/503/275/GCA_018503275.2_NA19240_mat_hprc_f2/GCA_018503275.2_NA19240_mat_hprc_f2_genomic.fna.gz
+
+#Rhesus Macaque:
+#GCF_003339765.1_Mmul_10
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/339/765/GCF_003339765.1_Mmul_10/GCF_003339765.1_Mmul_10_genomic.fna.gz
+
+#GCA_049350105.2_T2T-MMU8v2.0
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/350/105/GCF_049350105.2_T2T-MMU8v2.0/GCF_049350105.2_T2T-MMU8v2.0_genomic.fna.gz
